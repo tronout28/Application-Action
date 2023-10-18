e1a1b966ac631d76903b2eec97bfb5cc423d3218
tronout28 <fatihabdurahman28@gmail.com>
2023-10-18 13:12:19 +0700

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:application_action/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -10,7 +15,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-      return GetMaterialApp(
+    return GetMaterialApp(
       home: Scaffold(
         body: Center(
           child: Text('Hello World'),
